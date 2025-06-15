c5e9fe51919d09c816b78efdde7cdf3318b5a9bc
RadheTailor8336 <radhetailor923@gmail.com>
2025-06-16 00:36:20 +0530

    done

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,13 @@
 import Chai from "./chai";
 function App() {
- 
+  const username = "chai or code";
 
   return (
     <div>
        <Chai/>
 
-    <h1>chai or react</h1>
+    <h1>chai or react {username}</h1>  {/** evaluated expression */}
+    <p>test para</p>
     </div>
      
   )
