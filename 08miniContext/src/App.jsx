d594f8b0199e739b928a1c98bbf341cd087c85af
RadheTailor8336 <radhetailor923@gmail.com>
2025-06-20 22:41:12 +0530

    context api project

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
   
 
   return (
-    <>
+    <UserContextProvider>
       <h1>React with Chai and share is important</h1>
-    </>
+      <Login />
+      <Profile/>
+
+    </UserContextProvider>
   )
 }
 
