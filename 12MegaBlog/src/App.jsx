3e7ee937c133cb4be81c0148dcfa70e17deb2df5
RadheTailor8336 <radhetailor923@gmail.com>
2025-10-01 00:27:44 +0530

    Done

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log(c);
+  
+
+  return (
+    <>
+      <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
